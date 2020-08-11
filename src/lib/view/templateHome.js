c2a44fdedde7601d6bdcbf5ce9679c980ba912cd
CatalinaJL <catalina.p.jl@gmail.com>
2020-08-10 22:58:11 -0400

    Firebase y Log In con Google

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -1,10 +1,17 @@
+import { login } from '../index.js';
+
 export const home = () => {
   /* Creamos un DIV que contenga lo que creamos */
   const divHome = document.createElement('div');
   const viewHome = `
   <h1>Hola Mundirijillo</h1>
+  <button id="loginGoogle">Log In con Google</button>
   `;
   /* pasar al div creado con div home el viewHome */
   divHome.innerHTML = viewHome;
+  const btn = divHome.querySelector('#loginGoogle');
+  btn.addEventListener('click', () => {
+    login();
+  });
   return divHome;
 };
