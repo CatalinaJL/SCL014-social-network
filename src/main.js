1af3c2e17218c98fb2a3a34dbbfac43ed3959592
CatalinaJL <catalina.p.jl@gmail.com>
2020-08-13 12:14:05 -0400

    Revert "Cambios html main js y router js"
    This reverts commit 228a366d33c9f942335d8701f3e9cf5f394d6806.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 // Este es el punto de entrada de tu aplicacion
 import { myFunction } from './lib/index.js';
+<<<<<<< HEAD
+import { menu } from './lib/view/templateMenu.js';
+import { changeRoute } from './lib/router.js';
+// import { home } from './lib/view/templateHome.js';
+
+myFunction();
+
+//
+/* Si llamo de esta forma al div creado se imprime [object HTMLDivElement] */
+/* document.getElementById('root').innerHTML += home(); */
+/* document.getElementById('root').appendChild(home()); */
+
+/* Creamos función init que va a inicializar app */
+const init = () => {
+  /* Acá se va a pasar parametro a router.js */
+  document.getElementById('root').innerHTML = menu();
+  /* Window.addeventlistener va a ser el evento para cuando se cargue la pantalla */
+  window.addEventListener('hashchange', () => {
+    myFunction();
+    console.log(window.location.hash);
+    changeRoute(window.location.hash);
+  });
+};
+
+=======
 // import { logIn } from './lib/controllers/firebasecontroller.js';
 import { welcome } from './lib/view/WelcomeTemplate.js';
 import { changeRoute } from './lib/router.js';
@@ -13,4 +38,5 @@ const init = () => {
     changeRoute(window.location.hash);
   });
 };
+>>>>>>> 7506efe2f85522096abf8fa64464465a3380f29b
 window.addEventListener('load', init);
