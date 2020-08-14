45e4f4535b8b2ee5871c1459fd1c5512bb48de93
CatalinaJL <catalina.p.jl@gmail.com>
2020-08-14 19:10:46 -0400

    Archivos como venian de master, para empezar de nuevo video de SPA y routing

--- src/lib/view/templatePersonajes.js
+++
@@ -1,9 +0,0 @@
-export const personajes = () => {
-  const divPersonajes = document.createElement('div');
-  const viewPersonajes = `
-  <h3> Hola Flanders! </h3>
-  `;
-
-  divPersonajes.innerHTML = viewPersonajes;
-  return divPersonajes;
-};
